da6faf2b5d405e16923525bdf5edc17d8728240a
Hyugai1032 <rosaleskaentkraven@gmail.com>
2025-12-01 23:14:51 +0800

    Changed recaptcha key

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ body {
 
 // Load reCAPTCHA script dynamically to avoid blocking
 const recaptchaScript = document.createElement('script');
-recaptchaScript.src = 'https://www.google.com/recaptcha/api.js?render=6Leevh0sAAAAAFHXSDAziCYdUl4y30yFeQqrx3az';
+recaptchaScript.src = 'https://www.google.com/recaptcha/api.js?render=6Lfi7h0sAAAAAKJGT6TPB09Ivo9qw-9pSzA5Yx1u';
 document.head.appendChild(recaptchaScript);
 
 // Inject global styles
