452d16a37c43709f9428e677baea65f6cd25bcf0
Hyugai1032 <rosaleskaentkraven@gmail.com>
2025-12-03 09:22:01 +0800

    Duplicate

--- src/components/Cars/CarsManagement.vue
+++ src/components/Cars/CarsManagement.vue
@@ -631,7 +631,7 @@ const createCar = async () => {
     const payload = {
       ...form.value,
       additional_images: additionalImages.value.map(img => ({
-        url: img.image_url,
+        url: img.url,
         is_primary: img.is_primary ? 1 : 0   // o kaya !!img.is_primary
       }))
     }
